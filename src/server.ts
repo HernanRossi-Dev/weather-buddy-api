6434ab423b5943bcf232b1383f9da245ac60bb53
HernanRossi <hernan_rossi@msn.com>
2020-07-09 14:42:00 -0700

    Port 8080

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import { Server } from "http"
 import app from './app'
 import { logger } from "./utils";
 
-const PORT = 80;
+const PORT = 8080;
 let server: Server = app.listen(PORT, () => {
-  logger.debug('Application started on port 80.')
+  logger.debug('Application started on port 8080.')
 })
 
 export default server
